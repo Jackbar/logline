c9c31643bc01abf3b1b654d12102ac56e012f199
latel <latelx64@gmail.com>
2018-04-02 00:17:50 +0800

    add console plugin

---
+++ src/plugin/console.ts
@@ -0,0 +1,21 @@
+const LEVEL_CONSOLE_MAP = {
+    INFO: 'log',
+    WARN: 'warn',
+    ERROR: 'error',
+    CRITICAL: 'error'
+};
+
+export default {
+    name: 'console',
+    init() {
+        return typeof console !== 'undefined';
+    },
+    events: {
+        beforeRecord(data) {
+            console[LEVEL_CONSOLE_MAP[data.level.toUpperCase()] || LEVEL_CONSOLE_MAP.INFO](`${data.namespace} ${data.level.toUpperCase()} ${data.description}`, data.data);
+        },
+        error(error) {
+            console[LEVEL_CONSOLE_MAP.ERROR](`${error.identifier} ERROR ${error.description}`);
+        }
+    }
+};
